aebfab1ab19f729bab3b3a300e58aac9fe97d01e
frankw <frankwxx@126.com>
2026-04-14 13:21:22 +0800

    fix: use default dist directory

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 直接写死配置（Gitee Pages 不支持环境变量）
-const supabaseUrl = 'https://fbyddjtvazfrdtzczrig.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZieWRkanR2YXpmcmR0emN6cmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNDg1MDgsImV4cCI6MjA5MTYyNDUwOH0.JrQs4l12Q7ZOBxKP6JwejkpRm7NsquKhHER91NYPAw4'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
